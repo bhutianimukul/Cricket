0df00a9dfb5a953051299dca8d99c950c0908ac9
bhutianimukul <bhutianimukul@gmail.com>
2021-07-04 17:22:47 +0530

    did venue api task

---
+++ cricket/lib/Provider/venue_provider.dart
@@ -0,0 +1,28 @@
+import 'package:cricket/model/venue.dart';
+import 'package:flutter/material.dart';
+import "package:http/http.dart" as http;
+import "dart:convert";
+
+class VenueProvider with ChangeNotifier {
+  List<Venue> _venueList = [];
+  List<Venue> get venueList {
+    return [..._venueList];
+  }
+
+  Future<void> fetchVenue() async {
+    String url =
+        "https://cricket.sportmonks.com/api/v2.0/venues?api_token=NUlW0GSBmh9JUDIxSN0zvaUWY3cm5PNqHE6z0fL3f2CPifyrvs3PWg3v2SZ6";
+    final response = await http.get(Uri.parse(url));
+    final jsonData = json.decode(response.body);
+    final result = jsonData["data"] as List;
+    for (int i = 0; i < result.length; i++) {
+      Venue v = new Venue(
+          capacity: result[i]["capacity"],
+          name: result[i]["name"],
+          location: result[i]["city"],
+          imageUrl: result[i]["image_path"]);
+      _venueList.add(v);
+    }
+    notifyListeners();
+  }
+}
---
+++ cricket/lib/model/venue.dart
@@ -0,0 +1,12 @@
+class Venue {
+  String imageUrl;
+  String name;
+  String location;
+  int capacity;
+  Venue({
+    required this.capacity,
+    required this.name,
+    required this.location,
+    required this.imageUrl,
+  });
+}
