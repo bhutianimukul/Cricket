fca73813abb10fb499db3916a76b77b88a45fada
bhutianimukul <bhutianimukul@gmail.com>
2021-07-04 17:45:14 +0530

    corrected venue provider

--- cricket/lib/Provider/venue_provider.dart
+++ cricket/lib/Provider/venue_provider.dart
@@ -5,6 +5,7 @@ import "dart:convert";
 
 class VenueProvider with ChangeNotifier {
   List<Venue> _venueList = [];
+  List<String> _idList = [];
   List<Venue> get venueList {
     return [..._venueList];
   }
@@ -16,12 +17,27 @@ class VenueProvider with ChangeNotifier {
     final jsonData = json.decode(response.body);
     final result = jsonData["data"] as List;
     for (int i = 0; i < result.length; i++) {
-      Venue v = new Venue(
-          capacity: result[i]["capacity"],
-          name: result[i]["name"],
-          location: result[i]["city"],
-          imageUrl: result[i]["image_path"]);
-      _venueList.add(v);
+      _idList.add(result[i]["id"]);
+      if (i == 20) break;
+    }
+    fetch();
+  }
+
+  Future<void> fetch() async {
+    for (String id in _idList) {
+      String url =
+          "https://cricket.sportmonks.com/api/v2.0/venues/{$id}?api_token=NUlW0GSBmh9JUDIxSN0zvaUWY3cm5PNqHE6z0fL3f2CPifyrvs3PWg3v2SZ6";
+      final response = await http.get(Uri.parse(url));
+      final jsonData = json.decode(response.body);
+      final result = jsonData["data"] as List;
+      for (int i = 0; i < result.length; i++) {
+        Venue v = new Venue(
+            capacity: result[i]["capacity"],
+            name: result[i]["name"],
+            location: result[i]["city"],
+            imageUrl: result[i]["image_path"]);
+        _venueList.add(v);
+      }
     }
     notifyListeners();
   }
