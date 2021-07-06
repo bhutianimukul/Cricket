445a9ef139ee44ebdfb8425ab9c57c0393a87893
bhutianimukul <bhutianimukul@gmail.com>
2021-07-06 20:33:51 +0530

    added more records

--- cricket/lib/Provider/player_provider.dart
+++ cricket/lib/Provider/player_provider.dart
@@ -74,19 +74,97 @@ class PlayerProvider with ChangeNotifier {
       record["t20Runs"] = batting["T20Is"]["Runs"];
     else
       record["t20Runs"] = "0";
+    if (batting.containsKey("T20Is"))
+      record["t20100"] = batting["T20Is"]["100"];
+    else
+      record["t20100"] = "0";
+    if (batting.containsKey("T20Is"))
+      record["t2050"] = batting["T20Is"]["50"];
+    else
+      record["t2050"] = "0";
+    if (batting.containsKey("T20Is"))
+      record["t20Mat"] = batting["T20Is"]["Mat"];
+    else
+      record["t20Mat"] = "0";
+    if (batting.containsKey("T20Is"))
+      record["t20Avg"] = batting["T20Is"]["Ave"];
+    else
+      record["t20Avg"] = "0";
+    if (batting.containsKey("ODIs"))
+      record["odiAvg"] = batting["ODIs"]["Ave"];
+    else
+      record["odiAvg"] = "0";
+    if (batting.containsKey("ODIs"))
+      record["odiHs"] = batting["ODIs"]["HS"];
+    else
+      record["odiHs"] = "0";
+    if (batting.containsKey("ODIs"))
+      record["odi50"] = batting["ODIs"]["50"];
+    else
+      record["odi50"] = "0";
+    if (batting.containsKey("ODIs"))
+      record["odi100"] = batting["ODIs"]["100"];
+    else
+      record["odi100"] = "0";
+    if (batting.containsKey("T20Is"))
+      record["t20Hs"] = batting["T20Is"]["HS"];
+    else
+      record["t20Hs"] = "0";
     if (batting.containsKey("ODIs"))
       record["odiRuns"] = batting["ODIs"]["Runs"];
     else
       record["odiRuns"] = "0";
+    if (batting.containsKey("ODIs"))
+      record["odiMat"] = batting["ODIs"]["Mat"];
+    else
+      record["odiMat"] = "0";
     if (batting.containsKey("tests"))
       record["testRuns"] = batting["tests"]["Runs"];
     else
       record["testRuns"] = "0";
+    if (batting.containsKey("tests"))
+      record["testAvg"] = batting["tests"]["Ave"];
+    else
+      record["testAvg"] = "0";
+    if (batting.containsKey("tests"))
+      record["testMat"] = batting["tests"]["Mat"];
+    else
+      record["testMat"] = "0";
+    if (batting.containsKey("tests"))
+      record["testHs"] = batting["tests"]["HS"];
+    else
+      record["testHs"] = "0";
+    if (batting.containsKey("tests"))
+      record["test50"] = batting["tests"]["50"];
+    else
+      record["test50"] = "0";
+
+    if (batting.containsKey("tests"))
+      record["test100"] = batting["tests"]["100"];
+    else
+      record["test100"] = "0";
+
     final bowling = jsonData["bowling"];
     if (bowling.containsKey("T20Is"))
       record["t20W"] = bowling["T20Is"]["Wkts"];
     else
       record["t20RW"] = "0";
+    if (bowling.containsKey("T20Is"))
+      record["t20BBI"] = bowling["T20Is"]["BBI"];
+    else
+      record["t20BBI"] = "0";
+    if (bowling.containsKey("T20Is"))
+      record["t20Econ"] = bowling["T20Is"]["Econ"];
+    else
+      record["t20Econ"] = "0";
+    if (bowling.containsKey("T20Is"))
+      record["t20AvgW"] = bowling["T20Is"]["Ave"];
+    else
+      record["t20AvgW"] = "0";
+    if (bowling.containsKey("T20Is"))
+      record["t205w"] = bowling["T20Is"]["5w"];
+    else
+      record["t205w"] = "0";
     if (bowling.containsKey("ODIs"))
       record["odiW"] = bowling["ODIs"]["Wkts"];
     else
@@ -95,7 +173,39 @@ class PlayerProvider with ChangeNotifier {
       record["testW"] = bowling["tests"]["Wkts"];
     else
       record["testW"] = "0";
+    if (bowling.containsKey("tests"))
+      record["testBBI"] = bowling["tests"]["BBI"];
+    else
+      record["testBBI"] = "0";
+    if (bowling.containsKey("tests"))
+      record["testAvgW"] = bowling["tests"]["Ave"];
+    else
+      record["testAvgW"] = "0";
+    if (bowling.containsKey("tests"))
+      record["test5w"] = bowling["tests"]["5w"];
+    else
+      record["test5w"] = "0";
 
+    if (bowling.containsKey("tests"))
+      record["testEcon"] = bowling["tests"]["Econ"];
+    else
+      record["testEcon"] = "0";
+    if (bowling.containsKey("ODIs"))
+      record["odiAvgW"] = bowling["ODIs"]["Ave"];
+    else
+      record["odiAvgW"] = "0";
+    if (bowling.containsKey("ODIs"))
+      record["odiEcon"] = bowling["ODIs"]["Econ"];
+    else
+      record["odiEcon"] = "0";
+    if (bowling.containsKey("ODIs"))
+      record["odiBBI"] = bowling["ODIs"]["BBI"];
+    else
+      record["odiBBI"] = "0";
+    if (bowling.containsKey("ODIs"))
+      record["odi5w"] = bowling["ODIs"]["5w"];
+    else
+      record["odi5w"] = "0";
     return record;
   }
 
